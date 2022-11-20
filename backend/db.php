f80687d1987970ca702e65b8b520f444c4eec444
Samuel Owolabi <53657942+Samuel17715@users.noreply.github.com>
2022-11-20 18:11:03 +0100

    New

--- backend/db.php
+++ backend/db.php
@@ -1,19 +1,31 @@
 <?php
-	header("Access-Control-Allow-Origin: *");
-	
-	// Report all errors
-	error_reporting(E_ALL);
+header("Access-Control-Allow-Origin: *");
 
-	require_once ('server.php');
+// Report all errors
+error_reporting(E_ALL);
 
-    //PDO Database Configuration
-	$dbhost = 'localhost';	
-	$dbname = 'wavydawgs_waitlist';
-	$dbuser = 'root';	 
-	$dbpass = '';
+require_once('server.php');
 
-	// Creating a object of PDODatabaseConnection Class
-	$pdo = new serverProcessing($dbhost, $dbname, $dbuser, $dbpass);
+// //PDO Database Configuration
+// $dbhost = 'localhost';	
+// $dbname = 'wavydawgs_waitlist';
+// $dbuser = 'root';	 
+// $dbpass = '';
 
-    // Connect to the Database
-	$pdo->connect(); //If connection fails give error message and exit
+// //PDO Database Configuration
+// $dbhost = 'wavydawgswaitlist-do-user-12485871-0.b.db.ondigitalocean.com';	
+// $dbname = 'defaultdb';
+// $dbuser = 'doadmin';	 
+// $dbpass = 'AVNS_jJhWlL5vZ0m1RBTPUMf';
+
+//PDO Database Configuration
+$dbhost = '159.89.90.119';
+$dbname = 'wavydawgs_waitlist';
+$dbuser = 'admin';
+$dbpass = '95e5532b2f9faabe790786fa0bf2b29ae8ddb0abc0ce7613';
+
+// Creating a object of PDODatabaseConnection Class
+$pdo = new serverProcessing($dbhost, $dbname, $dbuser, $dbpass);
+
+// Connect to the Database
+$pdo->connect(); //If connection fails give error message and exit
